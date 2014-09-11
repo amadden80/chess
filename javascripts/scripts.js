f684b02bb76bada0e2c4afb12ea1324595131d4e
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-09-11 16:55:15 -0400

    chess board drawn

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,2 +1,28 @@
+console.log('Your Turn');
 
-console.log('Your Turn')
+var canvas;
+var context;
+var size = 80;
+
+window.onload = init;
+
+function init() {
+  canvas = document.getElementById('board');
+  context = canvas.getContext('2d');
+  console.log(context);
+  draw();
+}
+
+function drawBoard() {
+  for (var row = 0; row < 8; row++) {
+    for (var col = 0; col < 8; col++) {
+      context.fillStyle = (row + col) % 2 == 0 ? '#fff' : '#000';
+      context.fillRect(col * size, row * size, size, size);
+    }
+  }
+}
+
+function draw() {
+  context.clearRect(0, 0, 8 * size, 8 * size);
+  drawBoard();
+}
